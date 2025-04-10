864264e1731c2bd256e2e26109fd7146ab69893b
Sullivan <sullivan.espeut@laposte.net>
2025-04-10 16:13:37 +0200

    user

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/',(req,res)=> {
     res.json('Hello from workflow gitactions')
 })
 
+app.get('/user',(req,res)=> {
+    res.json('Hello from user')
+})
+
 app.listen('3001',()=>console.log('Server is running on 3001'))
